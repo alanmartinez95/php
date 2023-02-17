05af305a2755228c386371c1d158e4f10597cc20
alan martinez <alan.martinez.llamas.al@iespoligonosur.org>
2023-02-17 14:22:28 +0100

    ejercicio3.php

---
+++ ejercicio2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    for ($i=2; $i<=100; $i= $i+2){
+        echo $i."<br>";
+    }    
+
+    /*for ($acu=0, $acu=$acu+2 ){
+        echo $acu%2=0."<br>".; 
+    }*/
+    
+    
+    
+    ?>
+</body>
+</html>
